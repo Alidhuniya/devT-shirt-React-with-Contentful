b6c75f3a6b704797d41b7ad20bae01a7c29b5942
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-09-06 11:15:16 -0700

    responsive navabr added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from "react";
 import logo from "../assets/devswag-logo.svg";
 import "../sass/3-layout/_navbar.scss";
+import "../App.scss";
 import { Link } from "react-router-dom";
 
 export default class Navbar extends Component {
@@ -11,11 +12,18 @@ export default class Navbar extends Component {
 		
 		return (
 			<Fragment>
-			<div className = "navtop">
+			<div className = "navbar">
 			<header className = "header">
+			<input className="menu-btn" type="checkbox" id="menu-btn" />
+					<label className="menu-icon" for="menu-btn">
+						<span className="navicon"></span>
+					</label>
 				<div>
-					<img className = "header__logo" src= {logo} alt="logo" />
+					<Link to = "/" ><img className = "header__logo" src= {logo} alt="logo" /> </Link>
+					
 				</div>
+
+				
 
 				<nav className = "header__nav">
 					<li className = "header__navLink"><Link  to = "/">Home</Link></li>
@@ -25,8 +33,8 @@ export default class Navbar extends Component {
 				</nav>
 			</header>
 
-			</div>
-
+			
+</div>
 			</Fragment>
 		);
 	}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import App from "./App";
+// import App from "./App";
+import Navbar from './components/Navbar';
+
 import "./sass/basic-styles-scss/abstract-and-base.scss";
 
 
@@ -8,7 +10,7 @@ import {
   BrowserRouter as Router
 } from "react-router-dom";
  
-ReactDom.render(<Router><App /></Router>, document.getElementById("root"));
+ReactDom.render(<Router><Navbar /></Router>, document.getElementById("root"));
 
 
 if ('serviceWorker' in navigator) {
