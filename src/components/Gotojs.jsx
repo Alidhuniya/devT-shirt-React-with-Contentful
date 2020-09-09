fd8b9023522ce44fda0dc9287fed291c3ddb4dbf
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-09-08 22:06:09 -0700

    offer and js title added

--- src/components/Gotojs.jsx
+++ src/components/Gotojs.jsx
@@ -3,13 +3,21 @@ import { Switch, Route } from "react-router-dom";
 import Js from './Js';
 import Jsitem from './Jsitem';
 
+const offerheading = {
+  textAlign : "center",
+  fontSize : "2.5rem"
+}
+
 
 export default class Gotojs extends Component {
   render() {
     return (
       <Fragment>
        <Route exact path="/">
+       <div>
+       <h1 style = {offerheading}>JS</h1>
             <Js />
+            </div>
           </Route>
         <Switch>
           <Route exact path="/js/:id">
--- src/components/Gotother.jsx
+++ src/components/Gotother.jsx
@@ -2,7 +2,10 @@ import React, { Component, Fragment } from "react";
 import { Switch, Route } from "react-router-dom";
 import Offeritem from './Offeritem';
 import Other from './Other';
-
+const offerheading = {
+    textAlign : "center",
+    fontSize : "2.5rem"
+  }
 
 
 class Gotother extends Component {
@@ -11,7 +14,10 @@ class Gotother extends Component {
       <Fragment>
       
        <Route exact path="/">
+         <div>
+       <h1 style = {offerheading}>Other</h1>
             <Other />
+            </div>
           </Route>
         <Switch>
           <Route exact path="/offers/:id">
